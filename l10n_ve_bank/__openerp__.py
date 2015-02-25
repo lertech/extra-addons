6aa37aada03901dcb5ec1c81439a3ccfed744858
Francisco Lercari <flercari@lertech.com.ve>
2015-02-24 21:26:44 -0430

    [ADD] module bank Venezuela

---
+++ l10n_ve_bank/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Lertech <www.lertech.com.ve>
+#
+#    Modified: Francisco Lercari <flercari@lertech.com.ve>,
+#
+#    Created: 2014-01-16
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Venezuelan Bank',
+    'version': '0.1',
+    'author': 'LerTech',
+    'category': 'Localization',
+    'description': """
+Load Bank of Venezuela
+""",
+    'website': 'http://www.lertech.com.ve/',
+    'images' : [],
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'data/ve_bank.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
