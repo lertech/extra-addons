ec3f99a0e9fde6ea7a8d91fd04b2be922303c47a
Francisco Lercari <flercari@lertech.com.ve>
2015-03-30 12:09:03 -0430

    [FIX] dependencie ve city

--- l10n_ve_city/__init__.py
+++ l10n_ve_city/__init__.py
@@ -23,5 +23,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-import res_city
-import partner_address
+
