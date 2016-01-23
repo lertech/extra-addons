6f7b481b322bf0dd0ff9df7057dbe7cefb69203c
Franciscolercari <franciscolercari@gmail.com>
2016-01-23 11:13:18 -0430

    [REF] migrate odoo v8 module product historial price

--- product_historical_price/__init__.py
+++ product_historical_price/__init__.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# coding: utf-8
 ###########################################################################
 #    Module Writen to OpenERP, Open Source Management Solution
 #    Copyright (C) OpenERP Venezuela (<http://openerp.com.ve>).
@@ -20,5 +20,5 @@
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+##########################################################################
 from . import product
