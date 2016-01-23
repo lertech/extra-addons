e162d72d63a594e3f48ab56d02d5ebb2d1d2d4f7
Franciscolercari <franciscolercari@gmail.com>
2016-01-23 11:19:00 -0430

    [ADD] module sprint_kanban

---
+++ sprint_kanban/__openerp__.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+# -*- enconding: utf-8 -*-
+{
+    "name": "Sprint Kanban",
+    "version": "1.6",
+    "author": "Vauxoo",
+    "category": "Project",
+    "website": "http://vauxoo.com",
+    "license": "",
+    "depends": [
+        "project",
+        "web_kanban",
+        "base_setup",
+        "product",
+        "analytic",
+        "portal",
+        "board",
+        "mail",
+        "resource"
+    ],
+    "demo": [],
+    "data": [
+        "security/security_sprint_kanban.xml",
+        "security/ir.model.access.csv",
+        "view/sprint_kanban_view.xml",
+        "view/project_view.xml"
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+}
